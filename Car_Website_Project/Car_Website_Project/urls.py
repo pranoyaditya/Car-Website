357d68770ffd265926b797c57579b87a6c4a603d
pranoyaditya <pranoyaditya@gmail.com>
2024-12-16 23:10:39 +0600

    Filter button, car cards on the home page, and displaying the number of cars found have all been completed.

--- Car_Website_Project/Car_Website_Project/urls.py
+++ Car_Website_Project/Car_Website_Project/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home_page'),
+    path('<int:brand_id>/', HomeView.as_view(), name='home_page'),
     path('user/', include('User_App.urls')),
     path('car/', include('Car_App.urls')),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Car_Website_Project/Car_Website_Project/views.py
+++ Car_Website_Project/Car_Website_Project/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
-from Car_App.models import Brand
+from Car_App.models import Brand,Car
 
 # Class based views are written below.
 class HomeView(TemplateView):
@@ -8,8 +8,16 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        # Fetch all Brand objects from the database
-        context['brands'] = Brand.objects.all()  # Make sure 'brands' is passed to the template
+        brand_id = self.kwargs.get('brand_id',None)
+
+        if brand_id:
+            brand = Brand.objects.get(id = brand_id)
+            context['selected_brand'] = brand
+            context['cars'] = Car.objects.filter(brand = brand)
+        else:
+            context['selected_brand'] = None
+            context['cars'] = Car.objects.all()
+
+        context['brands'] = Brand.objects.all()
         
         return context
